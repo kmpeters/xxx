418e5f2fb02b8f6c663ab378a4a061981bb1736b
Kevin Peterson <kmpeters@anl.gov>
2016-10-21 16:16:45 -0500

    Explicitly define the EPICS_HOST_ARCH in start_ioc.bat since it will be needed most of the time

--- iocBoot/iocWindows/start_ioc.bat
+++ iocBoot/iocWindows/start_ioc.bat
@@ -2,7 +2,8 @@
 
 SETLOCAL
 
-REM set EPICS_HOST_ARCH=windows-x64
+REM This should match an existing subdirectory of the IOC's bin dir
+set EPICS_HOST_ARCH=windows-x64-static
 
 REM set EPICS_CA_MAX_ARRAY_BYTES=100000000
 
