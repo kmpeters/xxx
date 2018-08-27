e001e2239d46e6a6a38e0d4bd921bbf435f0bf8b
kpetersn <kmpeters@anl.gov>
2018-08-27 16:10:12 -0500

    Include motors.cmd and std.cmd by default in all IOCs.

--- iocBoot/iocxxx/cmds/std.cmd
+++ iocBoot/iocxxx/cmds/std.cmd
@@ -1,11 +1,11 @@
-# Soft scaler for testing
-#iocshLoad("$(STD)/iocsh/softScaler.iocsh", "P=$(PREFIX), INSTANCE=scaler1")
+# Soft scaler 
+#!iocshLoad("$(STD)/iocsh/softScaler.iocsh", "P=$(PREFIX), INSTANCE=scaler1")
 
 # pvHistory (in-crate archive of up to three PV's)
-#dbLoadRecords("$(STD)/stdApp/Db/pvHistory.db","P=$(PREFIX),N=1,MAXSAMPLES=1440")
+#!dbLoadRecords("$(STD)/stdApp/Db/pvHistory.db","P=$(PREFIX),N=1,MAXSAMPLES=1440")
 
 # software timer
-#dbLoadRecords("$(STD)/stdApp/Db/timer.db","P=$(PREFIX),N=1")
+#!dbLoadRecords("$(STD)/stdApp/Db/timer.db","P=$(PREFIX),N=1")
 
 # Miscellaneous PV's, such as burtResult
 dbLoadRecords("$(STD)/stdApp/Db/misc.db","P=$(PREFIX)")
