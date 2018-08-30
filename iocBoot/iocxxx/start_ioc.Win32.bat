3a4c3315932db9ea58b21b38c0ea8827a3087fe3
kpetersn <kmpeters@anl.gov>
2018-08-30 11:35:42 -0500

    Added start scripts for 32 and 64 bit Windows architectures.

---
+++ iocBoot/iocxxx/start_ioc.Win32.bat
@@ -0,0 +1,24 @@
+@echo OFF
+
+SETLOCAL
+
+REM This should match an existing subdirectory of the IOC's bin dir
+set EPICS_HOST_ARCH=windows-x32-static
+
+REM set EPICS_CA_MAX_ARRAY_BYTES=100000000
+
+REM Add caRepeater to the PATH
+REM set PATH=%PATH%;D:/epics/base-3.15.5/bin/%EPICS_HOST_ARCH%
+
+REM Go to the startup directory
+cd %~dp0
+
+REM dlls to the PATH
+call dllPath.bat
+
+REM start the IOC
+..\..\bin\%EPICS_HOST_ARCH%\xxx st.cmd.Win32
+
+pause
+
+ENDLOCAL
